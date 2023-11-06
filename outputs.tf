6b12ecdbaf5a7a7ba33ba662d6c7efb3a9b890bb
Jim McCann <jimccann@redhat.com>
2023-11-06 14:30:01 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-#output "virtual_machine" {
-#  description = "Returns host info for all deployed virtual instance."
-#  value       = module.virtual-machines[0].instance
-#  sensitive   = true
-#}
+output "virtual_machine" {
+  description = "Returns host info for all deployed virtual instance."
+  value       = module.virtual-machines[0].instance
+  sensitive   = true
+}
 
 output "private_vlan" {
   description = "Returns the private VLAN info."
